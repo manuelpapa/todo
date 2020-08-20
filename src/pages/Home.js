42faec7f376ac3a2708e33ff349ea67b515d9fef
manuelpapa <manulito@de-vio.com>
2020-08-20 10:42:50 +0200

    Add custom hook

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { fetchSamples } from "../api/fetchSamples";
 import { Link } from "react-router-dom";
+import useAsync from "../hooks/useAsync";
 
 export default function Home() {
-  const [samples, setSamples] = useState(null);
-  async function doFetch() {
-    const fetchData = await fetchSamples();
-    setSamples(fetchData);
-  }
-
-  useEffect(() => {
-    doFetch();
-  }, []);
+  const { data: samples, loading, error } = useAsync(fetchSamples);
 
   return (
     <div className="Home">
       <div className="Home-header">
         <div>
           <Link to="/add">Add Sample</Link>
+          {loading && <div>Loading...</div>}
         </div>
         <h2>Papas Samplebox</h2>
       </div>
+      {error && <div>ERROR!</div>}
+      {loading && <div>Loading...</div>}
       {samples?.map((sample) => (
         <div key={sample.id}>
           {sample.title} <i>by</i> {sample.artist} <i>in</i> {sample.genre}{" "}
